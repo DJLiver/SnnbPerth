1b407e669571b28b09460a9d32a310357563f1e4
David L <djlive455@gmail.com>
2023-06-19 10:27:33 +1200

    Sql script update

---
+++ BuildDBScript/SQLScripts/AllTables.sql
@@ -0,0 +1,13 @@
+/****** Put in Query -> SQLCMD mode ******/
+
+
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\H_SpectralNetGroups.sql
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\M_ControlNIc.sql
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\M_DataNIc.sql
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\M_Dependencies.sql
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\M_Modules.sql
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\M_MulticastGroupSubscriptions.sql
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\M_RfInputStream.sql
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\M_RfOutputStream.sql
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\M_Routes.sql
+:r C:\Projects\NarrowBand\SnnbFailover\BuildDBScript\SQLScripts\M_Spectrum.sql
