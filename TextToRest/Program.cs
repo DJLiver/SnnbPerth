4c8c031027bb45ae72a32c8b1bd58b3b7b03a12c
David Liversidge <djlive455@gmail.com>
2023-06-22 15:49:58 +1200

    Added TextToRest project

---
+++ TextToRest/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+using SnnbDB.Rest;
+
+using TextToRest;
+
+Console.WriteLine("Hello, World!");
+
+
+using (var sr = new StreamReader("sn.json"))
+{
+    // Read the stream as a string, and write the string to the console.
+    string s = sr.ReadToEnd();
+
+
+   // Rootobject sNModule = JsonConvert.DeserializeObject<Rootobject>(s);
+
+    SNModule sNModule = JsonConvert.DeserializeObject<SNModule>(s);
+
+
+}
