8bf118f143895a2f8cbab7f794e919c3ccec9949
David Liversidge <djlive455@gmail.com>
2023-07-03 18:01:41 +1200

    Added CommandTest

---
+++ CommandTest/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommandTest;
+
+Console.WriteLine("Hello, World!");
+#pragma warning disable CS0219 // Variable is assigned but its value is never used
+string RfOut = """{"rfOutputEnable": {"factory": "Attribute", "factoryType": "bool", "value": true }}""";
+
+string configName = """{"name": {"factory": "Attribute", "factoryType": "string", "value": "default" }}""";
+
+
+#pragma warning restore CS0219 // Variable is assigned but its value is never used
+
+//RestData.GetSNWBConfig("http://10.228.41.151", "rest/configService/_procedure/downloadConfigFile", configName);
+
+
+
+//RestData.GetConfigData("http://10.228.41.180", "rest/configService/_attribute?_dive=true");
+//RestData.GetSysHealthData("http://10.228.41.151", "rest/systemHealth/_attribute?_dive=true");
+//RestData.GetRestData("http://10.228.41.151", "rest/spectralNet/_attribute?_dive=true");
+//RestData.GetRestData("http://10.228.41.151", "rest/configService/_attribute?_dive=true");
+RestData.SetRestData("http://10.228.41.179", "/rest/spectralNet/_attribute/rfOutputEnable", RfOut);
---
+++ CommandTest/RestData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp;
+
+namespace CommandTest;
+public static class RestData
+{
+    public static void SetRestData(string IP, string command, string json)
+    {
+        var client = new RestClient(IP);
+
+        //var request = new CreateOrder()
+        var request = new RestRequest(command, Method.Post);
+
+
+        request.AddJsonBody(json);
+
+
+        RestResponse? response = client.Post(request);
+
+    }
+
+}
