4ffcacb8af785750495d9167c638580d0ee43290
David Liversidge <djlive455@gmail.com>
2023-08-20 20:27:51 +1200

    More charts

---
+++ Blazor/Client/Shared/SpectrumChart.razor.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using SnnbDB.ModelHub;
+
+namespace Failover.Client.Shared;
+
+
+public partial class SpectrumChart
+{
+    RtSpectrum Spectrum = new(true);
+
+    #region Injects
+    [Inject]
+    protected IJSRuntime JSRuntime { get; set; }
+
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    protected DialogService DialogService { get; set; }
+
+    [Inject]
+    protected TooltipService TooltipService { get; set; }
+
+    [Inject]
+    protected ContextMenuService ContextMenuService { get; set; }
+
+    [Inject]
+    protected NotificationService NotificationService { get; set; }
+
+    #endregion   
+
+
+}
